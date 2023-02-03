77d9cba3880e236d6887bfc95209023b39814ccd
Philip Tonaczew <tonaczew@gmail.com>
2023-02-03 17:19:50 +0100

    added js and css

--- main.js
+++ main.js
@@ -13,18 +13,27 @@ const addedProductContainer = document.getElementById("added-products-div");
 
 //Variables
 let finishedShoppingList;
-let responseArray = [];
+let responseArray;
 
 // Functions
+const increaseAmount = (price) => {
+  if (price !== "-") return Number(price);
+  else return 0;
+};
+
 const getAllShoppingListItems = () =>
   document.querySelectorAll(".shopping-item");
 
 const getData = async (parsedQuery) => {
   console.log(parsedQuery);
-  const response = await axios.get("http://localhost:8081/getPrice", {
-    params: { products: parsedQuery },
-  });
-  return response.data;
+  try {
+    const response = await axios.get("http://localhost:8081/getPrice", {
+      params: { products: parsedQuery },
+    });
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
 };
 
 const parseParam = (lista) => {
@@ -39,26 +48,39 @@ const parseParam = (lista) => {
 btnClear.addEventListener("click", () => {
   btnGetPrice.classList.add("disabled-btn");
   btnComplete.classList.remove("ready-btn");
+  userInputField.value = "";
   const shoppingItems = getAllShoppingListItems();
   while (responsListContainer.firstChild) {
     responsListContainer.removeChild(responsListContainer.firstChild);
   }
   shoppingItems.forEach((element) => element.parentNode.removeChild(element));
+
+  if (
+    shoppingResultContainer.contains(
+      document.getElementsByClassName("network-error-p")[0]
+    )
+  ) {
+    shoppingResultContainer.removeChild(
+      document.getElementsByClassName("network-error-p")[0]
+    );
+  }
 });
 
 userInputField.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     const paragraph = document.createElement("p");
     paragraph.className = "shopping-item";
-    paragraph.innerText = userInputField.value.toUpperCase();
+    paragraph.innerText = userInputField.value;
     addedProductContainer.appendChild(paragraph);
     userInputField.value = "";
+    paragraph.addEventListener("click", () => {
+      addedProductContainer.removeChild(paragraph);
+    });
   }
 });
 
 btnComplete.addEventListener("click", () => {
   finishedShoppingList = [];
-  console.log("before shopping list", finishedShoppingList);
   const shoppingItems = getAllShoppingListItems();
   for (let i = 0; i < shoppingItems.length; i++) {
     finishedShoppingList.push(shoppingItems[i].textContent);
@@ -69,39 +91,62 @@ btnComplete.addEventListener("click", () => {
 });
 
 btnGetPrice.addEventListener("click", async () => {
-  /*
+  btnGetPrice.classList.add("disabled-btn");
   // axios GET request to API
+
   const parsedQuery = parseParam(finishedShoppingList);
   responseArray = await getData(parsedQuery);
-  */
 
-  //looping array change for testing
-  const loopingArray = testObject;
+  if (typeof responseArray !== "string") {
+    //looping array change for testing
+    const loopingArray = responseArray;
 
-  for (let i = 0; i < loopingArray.length; i++) {
-    const divStore = document.createElement("div");
-    const productGridDiv = document.createElement("div");
-    divStore.classList = "store-div";
-    productGridDiv.classList = "product-div";
-    const paragraphStore = document.createElement("h2");
-    paragraphStore.innerText = loopingArray[i].store;
-    divStore.appendChild(paragraphStore);
-    divStore.appendChild(productGridDiv);
-    responsListContainer.appendChild(divStore);
+    for (let i = 0; i < loopingArray.length; i++) {
+      let amount = 0;
+      const divStore = document.createElement("div");
+      const productGridDiv = document.createElement("div");
+      divStore.classList = "store-div";
+      productGridDiv.classList = "product-div";
+      const paragraphStore = document.createElement("h2");
+      paragraphStore.innerText = loopingArray[i].store;
+      divStore.appendChild(paragraphStore);
+      divStore.appendChild(productGridDiv);
+      responsListContainer.appendChild(divStore);
 
-    for (let j = 0; j < loopingArray[i].shoppingList.length; j++) {
+      //refactor
+      for (let j = 0; j < loopingArray[i].shoppingList.length; j++) {
+        const divProductInfo = document.createElement("div");
+        divProductInfo.classList = "product-info-div";
+        const paragraphProduct = document.createElement("p");
+        const paragraphPrice = document.createElement("p");
+        paragraphProduct.innerText =
+          loopingArray[i].shoppingList[j].productName;
+        const productPrice = loopingArray[i].shoppingList[j].price;
+        amount += increaseAmount(productPrice);
+        paragraphPrice.innerText = `${productPrice} kr`;
+        divProductInfo.appendChild(paragraphProduct);
+        divProductInfo.appendChild(paragraphPrice);
+        productGridDiv.appendChild(divProductInfo);
+      }
       const divProductInfo = document.createElement("div");
       divProductInfo.classList = "product-info-div";
-      const paragraphProduct = document.createElement("p");
-      const paragraphPrice = document.createElement("p");
-      paragraphProduct.innerText = loopingArray[i].shoppingList[j].productName;
-      paragraphPrice.innerText = `${loopingArray[i].shoppingList[j].price} kr`;
-      divProductInfo.appendChild(paragraphProduct);
-      divProductInfo.appendChild(paragraphPrice);
+      const totalPriceLabel = document.createElement("p");
+      const totalPriceAmount = document.createElement("p");
+      totalPriceLabel.classList.add("tota-sum");
+      totalPriceAmount.classList.add("tota-sum");
+      totalPriceLabel.innerText = "Total";
+      totalPriceAmount.innerText = `${amount.toFixed(2)} kr`;
+      divProductInfo.appendChild(totalPriceLabel);
+      divProductInfo.appendChild(totalPriceAmount);
       productGridDiv.appendChild(divProductInfo);
     }
+  } else {
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("network-error-p");
+    paragraph.innerText = "Network error! Try again!";
+    shoppingResultContainer.appendChild(paragraph);
+    console.log("else:", responseArray);
   }
-  btnGetPrice.classList.add("disabled-btn");
 });
 
 const testObject = [
